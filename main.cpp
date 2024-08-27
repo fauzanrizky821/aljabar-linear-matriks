b0411175f1968d916c5dc3024c66c323f4824811
Fauzan Rizky Ramadhan <fauzanrizky821@gmail.com>
2024-08-27 17:03:46 +0700

    add: penjumlahan matriks (belum dibuat modul)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,72 @@
 #include <iostream>
-#include "matriks.h"
+// #include "matriks.h"
 
 using namespace std;
 
-int main (){
+void penjumlahan(int i, int j, int matriks1[][7], int matriks2[][7], int hasilMatriks[][7])
+{
+    if ((i != i) && (j != j))
+    {
+        cout << "Jumlah elemen pada matriks tidak sama. Matriks tidak dapat dijumlahkan!!" << endl;
+    }
+    else
+    {
+        for (int a = 0; a < 2; a++)
+        {
+            for (int b = 0; b < 2; b++)
+            {
+                hasilMatriks[a][b] = matriks1[a][b] + matriks2[a][b];
+            }
+        }
+    }
+}
 
+int main()
+{
+    int matriks1[2][2], matriks2[2][2], hasilMatriks[2][2];
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            matriks1[i][j] = 2;
+            matriks2[i][j] = 3;
+        }
+    }
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cout << matriks1[i][j];
+            cout << matriks2[i][j] << endl;
+        }
+    }
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cout << hasilMatriks[i][j];
+        }
+    }
+
+    // perkalian matriks dengan skalar
+    int skalar = 2;
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            hasilMatriks[i][j] = hasilMatriks[i][j] * skalar;
+        }
+    }
+
+    cout << "\nhasil perkalian matriks dengan skalar : " << endl;
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cout << hasilMatriks[i][j];
+        }
+    }
 }
--- matriks.h
+++ matriks.h
@@ -1,1 +1,14 @@
-#include "matriks.cpp"
+#ifndef MATRIKS_H
+#define MATRIKS_H
+
+// Include Library
+#include <iostream>
+
+using namespace std;
+
+
+
+// List of Moduls
+
+
+#endif
