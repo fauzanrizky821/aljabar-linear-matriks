fdf9732c9a0ffd18f3f01b8aff488a205fb89a4d
Fauzan Rizky Ramadhan <fauzanrizky821@gmail.com>
2024-08-31 15:34:23 +0700

    add perkalian

--- main.cpp
+++ main.cpp
@@ -21,35 +21,80 @@ void penjumlahan(int i, int j, int matriks1[][100], int matriks2[][100], int has
     }
 }
 
+void perkalian(int i1, int j1, int i2, int j2, int matriks1[][100], int matriks2[][100], int hasilMatriks[][100])
+{
+
+    int temp[i1];
+    int hasil = 1;
+    int ih = i1;
+    int jh = j2;
+
+    for (int i = 0; i < i1; i++)
+    {
+        temp[i] = 0;
+        cout << "Buat temp" << " " << i << ": " << temp[i] << endl;
+    }
+    cout << endl;
+
+    int m = 0;
+    for (int n; n < j2; n++)
+    {
+        m = 0;
+        for (int i = 0; i < i1; i++)
+        {
+            temp[i] = 0;
+            cout << "temp" << " " << i << ": " << temp[i] << endl;
+        }
+
+        for (int j = 0; j < j1; j++)
+        {
+            for (int i = 0; i < i1; i++)
+            {
+                temp[i] = temp[i] + matriks1[i][j] * matriks2[m][n];
+            }
+            m++;
+        }
+
+        cout << "Baris ke-" << n + 1 << endl;
+        for (int m = 0; m < ih; m++)
+        {
+            hasilMatriks[m][n] = temp[m];
+
+            cout << "Kolom ke-" << m + 1 << " " << hasilMatriks[m][n] << endl;
+        }
+    }
+}
+
 int main()
 {
-    int matriks1[2][2], matriks2[2][2], hasilMatriks[2][2];
+    int matriks1[100][100], matriks2[100][100], hasilMatriks[100][100];
 
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            matriks1[i][j] = 2;
-            matriks2[i][j] = 3;
+    matriks1[0][0] = 1;
+    matriks1[0][1] = 2;
+    matriks1[0][2] = 4;
+    matriks1[1][0] = 2;
+    matriks1[1][1] = 6;
+    matriks1[1][2] = 0;
+
+    matriks2[0][0] = 4;
+    matriks2[0][1] = 1;
+    matriks2[0][2] = 4;
+    matriks2[0][3] = 3;
+    matriks2[1][0] = 0;
+    matriks2[1][1] = -1;
+    matriks2[1][2] = 3;
+    matriks2[1][3] = 1;
+    matriks2[2][0] = 2;
+    matriks2[2][1] = 7;
+    matriks2[2][2] = 5;
+    matriks2[2][3] = 2;
+
+    perkalian(2, 3, 3, 4, matriks1, matriks2, hasilMatriks);
+
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 4; j++){
+            cout << hasilMatriks[i][j] << " ";
         }
+        cout << endl;
     }
-
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            cout << matriks1[i][j];
-            cout << matriks2[i][j] << endl;
-        }
-    }
-
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            cout << hasilMatriks[i][j];
-        }
-    }
-
-    
 }
