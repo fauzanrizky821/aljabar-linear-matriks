7cf27280588ae378b9938ac8dababb74ec53674e
Fauzan Rizky Ramadhan <fauzanrizky821@gmail.com>
2024-08-31 17:06:27 +0700

    add the author of modules

--- matriks.h
+++ matriks.h
@@ -3,23 +3,30 @@
 
 // Include Library
 #include <iostream>
+#include "matriks.cpp"
 
 using namespace std;
 
-// List of Moduls
+// List of Modules
+
+/* Fauzan */
 void penjumlahanMatriks(int i1, int j1, int i2, int j2, int matriks1[][100], int matriks2[][100], int hasilMatriks[][100]);
 void penguranganMatriks(int i1, int j1, int i2, int j2,int matriks1[][100], int matriks2[][100], int hasilMatriks[][100]);
 void perkalianLinear(int i1, int j1, int i2, int j2, int matriks1[][100], int matriks2[][100], int hasilMatriks[][100]);
 void inversMatriks(int i, int j, int matriks[][100], int hasilMatriks[][100]);
+
+/* Fachry */
 void transposeMatriks(int i, int j, int matriks[100][100]);
 void matriksNol(int i, int j);
+
+/* Wildan */
 void trace(int m, int n, int matriks[][100]);
 void perkalianSkalar(int skalar, int matriks[][100]);
 void buatMatriksIdentitas(int m, int n, int matriks[][100]);
+
+/* Rifqi */
 bool perkalianMatriks(int kolomA, int barisA, int kolomB, int barisB, int matriksA[100][100], int matriksB[100][100], int matriksC[100][100]);
 int gatePerkalian();
 int menampilkanMatriks(const char* nama_matriks, int kolom, int baris, int matriks[100][100]);
-
-#include "matriks.cpp"
 
 #endif
